f33731d81750ccfcfe5fc92f4a9612fd555b6f2a
unknown <carlos.viniciusufrpe@gmail.com>
2022-03-11 19:34:17 -0300

    realizada o código para webscraping do valor de mercado e divida liquida

--- index.js
+++ index.js
@@ -3,37 +3,32 @@ const puppeteer = require('puppeteer');
 console.log("Bem vindo ao conversor");
 async function rolarFim(page){
     await page.evaluate(() => {
-        return window.scrollTo(0,9000)
+        return window.scrollTo(0,9500)
       });
 }
-
-function transformarNumero(valor){
-    numero = "";
-    for(let i = 0; i<valor.length;i++){
-        if(valor[i] != " " || valor[i]!="M" || valor[i] != "B"){
-            numero+= valor[i]
-        };
-    };
-    numero = numero.replace(",", ".");
-    return parseFloat(numero)
+function tratarNumero(valor){
+    valor = valor.replaceAll(".","");
+    valor = valor.replaceAll(",",".")
+    return valor
 }
-
 async function webScraping(){
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto('https://statusinvest.com.br/acoes/cgra4');
+    await page.goto('https://statusinvest.com.br/acoes/petr4');
     await rolarFim(page)
     await page.waitForSelector('.level-0.value.text-right.DATA.timeType-1');
     const RESULTADO =  await page.evaluate(()=>{
         return {
             ebit:document.querySelectorAll('.level-0.value.text-right.DATA.timeType-1')[6].children[0].innerHTML,
-            valor: 2,
-            divida: 3
+            valor: document.querySelectorAll('.top-info.info-3.sm.d-flex.justify-between.mb-3')[0].children[6].children[0].children[0].children[2].innerHTML,
+            divida: document.querySelector(".top-info.info-3.sm.d-flex.justify-between.mb-3").children[5].children[0].children[0].children[2].innerHTML
         }
     });
     
-    //await browser.close();
-   console.log(transformarNumero(RESULTADO.ebit))
+    await browser.close();
+    console.log(parseFloat(tratarNumero(RESULTADO.ebit)));
+    console.log(tratarNumero(RESULTADO.valor));
+    console.log(tratarNumero(RESULTADO.divida));
 }
 
 webScraping();
